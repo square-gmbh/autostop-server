07994306ebefc3e076df347d1993c256d619676f
Luca Boieru <luca.boieru@yahoo.com>
2014-06-13 12:09:25 +0300

    Json parse on server.

--- modules/route_handler/operations.js
+++ modules/route_handler/operations.js
@@ -1,8 +1,8 @@
 exports.test = function (link) {
 
-	console.log(link.data);
+	var data = JSON.parse(link.data);
 
 	link.res.setHeader('Access-Control-Allow-Origin', link.headers.origin);
 	link.res.writeHead(200, {'Content-Type': 'text/plain'});
-	link.res.end("Hello! " + link.data.toString());
+	link.res.end("Hello! " + JSONStringify(data));
 };
