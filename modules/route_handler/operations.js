042b31985b38836365d295ae77049eaf9ac1548a
Dan Andrei <andrei_dani_96@yahoo.com>
2014-06-22 21:56:41 +0300

    added search operation

--- modules/route_handler/operations.js
+++ modules/route_handler/operations.js
@@ -13,12 +13,51 @@ var ObjectId = require('mongodb').ObjectID;
 *   }
 */
 exports.search = function (link) {
-
-    console.log(link.data);
-
     link.res.setHeader('Access-Control-Allow-Origin', link.headers.origin);
-    link.res.writeHead(200, {'Content-Type': 'text/plain'});
-    link.res.end(JSON.stringify(link.data));
+
+    var route = link.data;
+    if (!route.start || !route.destination || !route.date) {
+        link.res.writeHead(400);
+        link.res.end('ERR_BAD_ROUTE');
+        return;
+    }
+
+    // start the search operation
+    getCollection('routes', function (err, col) {
+
+        if (err) {
+            link.res.writeHead(500);
+            link.res.end(JSON.stringify(err));
+            return;
+        }
+
+        //STAGE I
+        // using start and destination
+        col.find({ 'start': route.start, 'destination': route.destination, 'date': route.date}).toArray(function (err, routes1) {
+
+            if (err) {
+                link.res.writeHead(500);
+                link.res.end(JSON.stringify(err));
+                return;
+            }
+
+            //STAGE II
+            // using via
+            col.find({ 'via': { $in : [route.start, route.destination] }}).toArray(function (err, routes2) {
+
+                if (err) {
+                    link.res.writeHead(500);
+                    link.res.end(JSON.stringify(err));
+                    return;
+                }
+
+                // merge the 2 results
+                var result = mergeRoutes(routes1, routes2);
+                link.res.writeHead(200);
+                link.res.end(JSON.stringify(result));
+            });
+        });
+    });
 };
 
 /* this function creates routes */
@@ -39,6 +78,16 @@ exports.search = function (link) {
 exports.create = function (link) {
     link.res.setHeader('Access-Control-Allow-Origin', link.headers.origin);
 
+    // get the route
+    var route = link.data;
+
+    // validate the route
+    if (!route.start || !route.destination || !route.date || !route.seats) {
+        link.res.writeHead(400);
+        link.res.end('ERR_BAD_ROUTE');
+        return;
+    }
+
     // connect to mongo
     getCollection('routes', function (err, col) {
 
@@ -48,14 +97,14 @@ exports.create = function (link) {
             return;
         }
 
-        // get the route
-        var route = link.data;
+        // handle via
+        if (route.via) {
+            route.via = route.via.split(',');
 
-        // validate the route
-        if (!route.start || !route.destination || !route.date || !route.seats) {
-            link.res.writeHead(400);
-            link.res.end('ERR_BAD_ROUTE');
-            return;
+            // trim
+            for (var i in route.via) {
+                route.via[i].trim();
+            }
         }
 
         // insert the route
@@ -89,4 +138,30 @@ function getCollection (collection_name, callback) {
 
         callback(null, collection);
     });
+}
+
+// merges the 2 stages of the route search operation preventing duplicates
+// private function
+function mergeRoutes (routes1, routes2) {
+
+    var result = [];
+    result = routes1;
+
+    // add the 2 stage routes to the array but check for duplicates
+    for (var i in routes2) {
+
+        var found = false;
+        for (var j in result) {
+            if (result[j]._id.toString() === routes2[i]._id.toString()) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            result.push(routes2[i]);
+        }
+    }
+
+    return result;
 }
